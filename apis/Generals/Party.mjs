2d73f81bc16d84136459c14ebe0b42322b3430e5
Hamzaali565 <ha6153806@gmail.com>
2023-09-21 23:15:28 +0500

    Still working on it

--- apis/Generals/Party.mjs
+++ apis/Generals/Party.mjs
@@ -6,12 +6,19 @@ router.post("/addparty", async (req, res) => {
   try {
     let { parent, childs } = req.body;
     if (!parent) throw new Error("Parent is required");
-
+    // parent Check
+    const checkParent = await PartyModel.find({ parent });
+    console.log(checkParent);
+    // goes inside if there is not child in body
     if (!childs || childs.length <= 0) {
+      if (checkParent.length > 0) throw new Error("Parent Already Exist");
       const response = await PartyModel.create({ parent });
       res.status(200).send({ data: response });
+      console.log("no child");
       return;
-    } else if (childs.length > 0) {
+    }
+    // goes inside if there is child in body
+    else if (childs.length > 0) {
       let conditionCheck = true;
       for (const items of childs) {
         if (!items?.name) {
@@ -25,7 +32,16 @@ router.post("/addparty", async (req, res) => {
           { $push: { childs } },
           { new: true }
         );
-        res.status(200).send({ data: response });
+        if (response !== null) {
+          res.status(200).send({ data: response });
+          return;
+        }
+        console.log("is child", response);
+        if (response === null) {
+          const createBoth = await PartyModel.create({ parent, childs });
+          res.status(200).send({ data: createBoth });
+          return;
+        }
       } else {
         const response = await PartyModel.create({ parent });
         res.status(200).send({ data: response });
