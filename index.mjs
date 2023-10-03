e79c402c9738ae82f4509232d1bb5f48f6c81651
Hamzaali565 <ha6153806@gmail.com>
2023-10-03 20:03:34 +0500

    Unit created

--- index.mjs
+++ index.mjs
@@ -14,6 +14,7 @@ import SubCost from "./apis/Generals/SubCostCenter.mjs";
 import CostHead from "./apis/Generals/CostHead.mjs";
 import PaymentTerm from "./apis/Generals/PaymentTerm.mjs";
 import Country from "./apis/Generals/Country.mjs";
+import Unit from "./apis/Generals/Unit.mjs";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
@@ -43,6 +44,7 @@ app.use("/api/v1", SubCost);
 app.use("/api/v1", CostHead);
 app.use("/api/v1", PaymentTerm);
 app.use("/api/v1", Country);
+app.use("/api/v1", Unit);
 
 const __dirname = path.resolve();
 app.use("/", express.static(path.join(__dirname, "./Frontend/build")));
