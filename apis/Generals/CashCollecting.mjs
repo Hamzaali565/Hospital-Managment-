68a89e7c9939c89c823df4e850df114ed70f1b48
Hamzaali565 <ha6153806@gmail.com>
2024-04-18 16:30:34 +0500

    vector query completed for cash collecting location

--- apis/Generals/CashCollecting.mjs
+++ apis/Generals/CashCollecting.mjs
@@ -5,13 +5,29 @@ const router = express.Router();
 
 router.post("/cashlocation", async (req, res) => {
   try {
-    const { code, description } = req.body;
-    if (![code, description].every(Boolean))
+    const { name, status } = req.body;
+    if (![name, status].every(Boolean))
       throw new Error("All Parameters Are Required");
-    const response = await cashCollectingModel.create({ code, description });
+    const response = await cashCollectingModel.create({
+      name,
+      status,
+    });
     res
       .status(201)
       .json({ message: `New Cash Location Created`, data: response });
+  } catch (error) {
+    res.status(400).send({ message: `${error.message}` });
+  }
+});
+
+router.get("/cashlocation", async (req, res) => {
+  try {
+    const { name } = req.query;
+    if (!name) throw new Error("Name is required");
+    const response = await cashCollectingModel.find({
+      name: { $regex: new RegExp(name, "i") },
+    });
+    res.status(201).json({ data: response });
   } catch (error) {
     res.status(400).send({ message: `${error.message}` });
   }
--- dbRepo/CashCollectingModel.mjs
+++ dbRepo/CashCollectingModel.mjs
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
+import AutoIncrementFactory from "mongoose-sequence";
 
-const cashCollecting = new mongoose.Schema({
-  code: { type: String, required: true },
-  description: { type: String, required: true },
+const AutoIncrement = AutoIncrementFactory(mongoose);
+
+const { Schema } = mongoose;
+
+const cashCollectingSchema = new Schema({
+  code: { type: Number, unique: true }, // Change type to Number
+  name: { type: String, required: true },
+  status: { type: Boolean, default: false },
 });
+
+cashCollectingSchema.plugin(AutoIncrement, { inc_field: "code" });
+
 export const cashCollectingModel = mongoose.model(
   "cashCollecting",
-  cashCollecting
+  cashCollectingSchema
 );
